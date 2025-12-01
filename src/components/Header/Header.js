a38caede18c2ded33a1a95e358e95b77bf84bcc2
John Doe <mahdibenalaya566@gmail.com>
2025-12-01 20:47:28 +0100

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./header.css"
+import BasicExample from '../card/Card'
+
+
+function Header() {
+  return (
+    <div className='header' >  
+        <BasicExample/>
+        <BasicExample/>
+        <BasicExample/>
+        <BasicExample/>
+    </div>
+  )
+}
+
+export default Header
