747135940aff513bf9a6d05d5048ae5554f3e0ee
John Doe <mahdibenalaya566@gmail.com>
2025-12-05 20:51:28 +0100

    jsx

---
+++ src/product.js
@@ -0,0 +1,6 @@
+ export const product = {
+    "name": "samsung",
+    "description": "Écran 6.7 PLS LCD (2400x 1080px, FHD+), 90Hz - Système d'exploitation: Android 13 - Processeur: Snapdragon 680 4G (6 nm) (2.4GHz , 1.9GHz) - Mémoire ...",
+    "price": "2000",
+    "url": "https://informatica.tn/wp-content/uploads/2024/04/smartphone-samsung-galaxy-a05s-6go-128go-violet.jpg"
+}
